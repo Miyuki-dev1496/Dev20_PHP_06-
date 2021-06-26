6a3dccef1fe9c98f4105fc718a5c60b5826d8e93
Miyuki-dev1496 <83995983+Miyuki-dev1496@users.noreply.github.com>
2021-06-26 10:15:34 +0900

    Add files via upload

---
+++ myPage/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="login_act.php">
+        <div class="login">
+        <fieldset>
+            <label for="">ID : <input type="text" name="lid">  </label>
+            <label for="">PW : <input type="text" name="lpw">  </label>
+            <input type="submit" value="ログイン">
+        </fieldset>
+        </div>
+    </form>
+
+    <p>新規登録の方は<a href="signup.php">こちら</a></p>
+</body>
+</html>
